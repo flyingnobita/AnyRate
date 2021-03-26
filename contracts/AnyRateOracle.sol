94696c054b1fb7b3f468469625933054f858a051
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2021-03-26 16:37:28 +0800

    feat: *not compiling* adding AnyRateOracle

---
+++ contracts/AnyRateOracle.sol
@@ -0,0 +1,180 @@
+/** This example code is designed to quickly deploy an example contract using Remix.
+ *  If you have never used Remix, try our example walkthrough: https://docs.chain.link/docs/example-walkthrough
+ *  You will need testnet ETH and LINK.
+ *     - Kovan ETH faucet: https://faucet.kovan.network/
+ *     - Kovan LINK faucet: https://kovan.chain.link/
+ */
+
+pragma solidity ^0.6.6;
+
+//import "https://raw.githubusercontent.com/smartcontractkit/chainlink/develop/evm-contracts/src/v0.6/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title AnyRateOracle is an example contract which requests data from
+ * the Chainlink network
+ * @dev This contract is designed to work on multiple networks, including
+ * local test networks
+ */
+contract AnyRateOracle is ChainlinkClient, Ownable {
+    string public name = "AnyRateOracle";
+    uint256 public usage;
+    uint256 public unitCost;
+
+    /**
+     * @notice Deploy the contract with a specified address for the LINK
+     * and Oracle contract addresses
+     * @dev Sets the storage for the specified addresses
+     * @param _link The address of the LINK token contract
+     */
+    constructor(address _link) public {
+        if (_link == address(0)) {
+            setPublicChainlinkToken();
+        } else {
+            setChainlinkToken(_link);
+        }
+    }
+
+    function getUsage() public view returns (uint256) {
+        return usage;
+    }
+
+    function getUnitCost() public view returns (uint256) {
+        return unitCost;
+    }
+
+    /**
+     * @notice Creates a request to the specified Oracle contract address
+     * @dev This function ignores the stored Oracle contract address and
+     * will instead send the request to the address specified
+     * @param _oracle The Oracle contract address to send the request to
+     * @param _jobId The bytes32 JobID to be executed
+     * @param _url The URL to fetch data from
+     * @param _path The dot-delimited path to parse of the response
+     * @param _times The number to multiply the result by
+     */
+    function createRequestForUsage(
+        address _oracle,
+        bytes32 _jobId,
+        uint256 _payment,
+        string memory _url,
+        string memory _path,
+        int256 _times
+    ) public onlyOwner returns (bytes32 requestId) {
+        Chainlink.Request memory req =
+            buildChainlinkRequest(
+                _jobId,
+                address(this),
+                this.fulfillUsage.selector
+            );
+        req.add("url", _url);
+        req.add("path", _path);
+        req.addInt("times", _times);
+        requestId = sendChainlinkRequestTo(_oracle, req, _payment);
+    }
+
+    /**
+     * @notice The fulfill method from requests created by this contract
+     * @dev The recordChainlinkFulfillment protects this function from being called
+     * by anyone other than the oracle address that the request was sent to
+     * @param _requestId The ID that was generated for the request
+     * @param _usage The answer provided by the oracle
+     */
+    function fulfillUsage(bytes32 _requestId, uint256 _usage)
+        public
+        recordChainlinkFulfillment(_requestId)
+        returns (uint256)
+    {
+        usage = _usage;
+        return usage;
+    }
+
+    /**
+     * @notice Creates a request to the specified Oracle contract address
+     * @dev This function ignores the stored Oracle contract address and
+     * will instead send the request to the address specified
+     * @param _oracle The Oracle contract address to send the request to
+     * @param _jobId The bytes32 JobID to be executed
+     * @param _url The URL to fetch data from
+     * @param _path The dot-delimited path to parse of the response
+     * @param _times The number to multiply the result by
+     */
+    function createRequestForUnitCost(
+        address _oracle,
+        bytes32 _jobId,
+        uint256 _payment,
+        string memory _url,
+        string memory _path,
+        int256 _times
+    ) public onlyOwner returns (bytes32 requestId) {
+        Chainlink.Request memory req =
+            buildChainlinkRequest(
+                _jobId,
+                address(this),
+                this.fulfillUnitCost.selector
+            );
+        req.add("url", _url);
+        req.add("path", _path);
+        req.addInt("times", _times);
+        requestId = sendChainlinkRequestTo(_oracle, req, _payment);
+    }
+
+    /**
+     * @notice The fulfill method from requests created by this contract
+     * @dev The recordChainlinkFulfillment protects this function from being called
+     * by anyone other than the oracle address that the request was sent to
+     * @param _requestId The ID that was generated for the request
+     * @param _unitCost The answer provided by the oracle
+     */
+    function fulfillUnitCost(bytes32 _requestId, uint256 _unitCost)
+        public
+        recordChainlinkFulfillment(_requestId)
+        returns (uint256)
+    {
+        unitCost = _unitCost;
+        return unitCost;
+    }
+
+    /**
+     * @notice Returns the address of the LINK token
+     * @dev This is the public implementation for chainlinkTokenAddress, which is
+     * an internal method of the ChainlinkClient contract
+     */
+    function getChainlinkToken() public view returns (address) {
+        return chainlinkTokenAddress();
+    }
+
+    /**
+     * @notice Allows the owner to withdraw any LINK balance on the contract
+     */
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+
+    /**
+     * @notice Call this method if no response is received within 5 minutes
+     * @param _requestId The ID that was generated for the request to cancel
+     * @param _payment The payment specified for the request to cancel
+     * @param _callbackFunctionId The bytes4 callback function ID specified for
+     * the request to cancel
+     * @param _expiration The expiration generated for the request to cancel
+     */
+    function cancelRequest(
+        bytes32 _requestId,
+        uint256 _payment,
+        bytes4 _callbackFunctionId,
+        uint256 _expiration
+    ) public onlyOwner {
+        cancelChainlinkRequest(
+            _requestId,
+            _payment,
+            _callbackFunctionId,
+            _expiration
+        );
+    }
+}
