538841b0ba28e2e39625e363c17e0e2e2c47323d
Sam Hodak <slhodak@gmail.com>
2021-03-27 18:03:14 -0700

    Begin user app web3 connection

--- UserApp/server/index.js
+++ UserApp/server/index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 4321;
 
 app.use(express.static('public'));
 
+// GET balance from Billing contract
+
+// POST funds to billing account
+
 app.listen(port, () => {
   console.log(`User app listening on port ${port}`);
 });
