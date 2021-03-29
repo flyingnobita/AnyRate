6b2b310eb1cf313762562a7e1a92053dac8524bf
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2021-03-29 22:41:09 +0800

    feat: add router & burger menu

--- client_app_cea/packages/react-app/src/App.js
+++ client_app_cea/packages/react-app/src/App.js
@@ -1,73 +1,107 @@
-import React from "react";
-import { Contract } from "@ethersproject/contracts";
-import { getDefaultProvider } from "@ethersproject/providers";
 import { useQuery } from "@apollo/react-hooks";
-
-import { Body, Button, Header, Image, Link } from "./components";
-import logo from "./ethereumLogo.png";
-import useWeb3Modal from "./hooks/useWeb3Modal";
-
-import { addresses, abis } from "@project/contracts";
+// import { Main, textStyle } from "@aragon/ui";
+// import { Web3Provider } from "@ethersproject/providers";
+// import { Web3ReactProvider } from "@web3-react/core";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { UseWalletProvider } from "use-wallet";
+// import Navigation from "./components/Navigation";
+// import Web3React from "./components/web3-react";
 import GET_TRANSFERS from "./graphql/subgraph";
+// import AnyRate from "./pages/AnyRate";
+import Landing from "./pages/Landing";
 
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = getDefaultProvider();
-  // Create an instance of an ethers.js Contract
-  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
-  // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
-  console.log({ tokenBalance: tokenBalance.toString() });
-}
+import React, { useState, useRef } from "react";
+import { ThemeProvider } from "styled-components";
+import { useOnClickOutside } from "./hooks";
+import { GlobalStyles } from "./global";
+import { theme } from "./theme";
+import { Burger, Menu } from "./components";
+import FocusLock from "react-focus-lock";
 
-function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
-  return (
-    <Button
-      onClick={() => {
-        if (!provider) {
-          loadWeb3Modal();
-        } else {
-          logoutOfWeb3Modal();
-        }
-      }}
-    >
-      {!provider ? "Connect Wallet" : "Disconnect Wallet"}
-    </Button>
-  );
-}
+require("dotenv").config();
+
+// function getLibrary(provider) {
+//   const library = new Web3Provider(provider);
+//   library.pollingInterval = 12000;
+//   return library;
+// }
+
+// function App() {
+//   const { loading, error, data } = useQuery(GET_TRANSFERS);
+
+//   React.useEffect(() => {
+//     if (!loading && !error && data && data.transfers) {
+//       console.log({ transfers: data.transfers });
+//     }
+//   }, [loading, error, data]);
+
+//   return (
+//     <Web3ReactProvider getLibrary={getLibrary}>
+//       <UseWalletProvider
+//         chainId={31337}
+//         connectors={{
+//           injected: {},
+//         }}
+//       >
+//         <BrowserRouter>
+//           <Main>
+//             <div
+//               css={`
+//                 ${textStyle("title1")};
+//               `}
+//             >
+//               Test Title 1
+//             </div>
+//             <Navigation />
+//             {/* <Layout> */}
+//             <Switch>
+//               <Route exact path={"/"} component={Landing} />
+//             </Switch>
+//             {/* </Layout> */}
+//           </Main>
+//         </BrowserRouter>
+//       </UseWalletProvider>
+//     </Web3ReactProvider>
+//   );
+// }
 
 function App() {
-  const { loading, error, data } = useQuery(GET_TRANSFERS);
-  const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  const menuId = "main-menu";
 
-  React.useEffect(() => {
-    if (!loading && !error && data && data.transfers) {
-      console.log({ transfers: data.transfers });
-    }
-  }, [loading, error, data]);
+  useOnClickOutside(node, () => setOpen(false));
 
   return (
-    <div>
-      <Header>
-        <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal} />
-      </Header>
-      <Body>
-        <Image src={logo} alt="react-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
-        <Button hidden onClick={() => readOnChainData()}>
-          Read On-Chain Balance
-        </Button>
-        <Link href="https://ethereum.org/developers/#getting-started" style={{ marginTop: "8px" }}>
-          Learn Ethereum
-        </Link>
-        <Link href="https://reactjs.org">Learn React</Link>
-        <Link href="https://thegraph.com/docs/quick-start">Learn The Graph</Link>
-      </Body>
-    </div>
+    // <Web3ReactProvider getLibrary={getLibrary}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyles />
+          <div ref={node}>
+            <FocusLock disabled={!open}>
+              <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+              <Menu open={open} setOpen={setOpen} id={menuId} />
+            </FocusLock>
+          </div>
+          <div>
+            <h1>Hello. This is burger menu tutorial</h1>
+            <img
+              src="https://image.flaticon.com/icons/svg/2016/2016012.svg"
+              alt="burger icon"
+            />
+            <small>
+              Icon made by <a href="https://www.freepik.com/home">Freepik</a>{" "}
+              from <a href="https://www.flaticon.com">www.flaticon.com</a>
+            </small>
+          </div>
+          <Switch>
+            <Route exact path={"/"} component={Landing} />
+          </Switch>
+        </>
+      </ThemeProvider>
+    </BrowserRouter>
+    // </Web3ReactProvider>
   );
 }
 
---
+++ client_app_cea/packages/react-app/src/pages/Landing/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <React.Fragment>
+      <h1>Landing</h1>
+      {/* <h1>AnyRate</h1>
+  <div><em>Blockchain Billing</em></div>
+  <h2>Sources</h2>
+  <hr>
+  <h3>Source Name</h3>
+  <input type="text" />
+  <h3>Type</h3>
+  <select>
+    <option>Quantitative</option>
+    <option>Categorical</option>
+  </select>
+  <h2>Condition</h2>
+  <h3>Threshold (if type == quantitative)</h3>
+  <input type="number">
+  <h3>Value (if type == categorical)</h3>
+  <input type="text">
+  <h2>Action</h2>
+  <select>
+    <option>Debit</option>
+    <option>Credit</option>
+    <option>Notify</option>
+  </select>
+  <div>
+    Rate <input type="number">
+  </div>
+  <div>
+    Amount<input type="number">
+  </div>
+  <div>
+    API URL <input type="text">
+  </div> */}
+    </React.Fragment>
+  );
+};
+
+export default Landing;
