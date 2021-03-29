38b1047c2f17df37827eb1c5206f994db7830e50
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2021-03-30 00:17:09 +0800

    feat: update menu options

--- client_app_cea/packages/react-app/src/components/Menu/Menu.js
+++ client_app_cea/packages/react-app/src/components/Menu/Menu.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { bool } from 'prop-types';
-import { StyledMenu } from './Menu.styled';
+import { bool } from "prop-types";
+import React from "react";
+import { StyledMenu } from "./Menu.styled";
 
 const Menu = ({ open, ...props }) => {
-  
   const isHidden = open ? true : false;
   const tabIndex = isHidden ? 0 : -1;
 
@@ -11,22 +10,14 @@ const Menu = ({ open, ...props }) => {
     <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
       <a href="/" tabIndex={tabIndex}>
         <span aria-hidden="true">💁🏻‍♂️</span>
-        About us
+        Home
       </a>
-      <a href="/" tabIndex={tabIndex}>
-        <span aria-hidden="true">💸</span>
-        Pricing
-        </a>
-      <a href="/" tabIndex={tabIndex}>
-        <span aria-hidden="true">📩</span>
-        Contact
-        </a>
     </StyledMenu>
-  )
-}
+  );
+};
 
 Menu.propTypes = {
   open: bool.isRequired,
-}
+};
 
 export default Menu;
