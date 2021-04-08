02871b6a0e39b346628fd2a38d45e821ca89c697
Sam Hodak <slhodak@gmail.com>
2021-04-07 21:40:27 -0700

    More User App

--- client_app_cea/packages/react-app/src/pages/User/index.tsx
+++ client_app_cea/packages/react-app/src/pages/User/index.tsx
@@ -1,62 +1,109 @@
-import React, { useEffect, useState } from "react";
+import { abis, addresses } from "@project/contracts";
+import { ethers } from "ethers";
+import { useWeb3React } from "@web3-react/core";
+import React from "react";
 import {
   Box,
   Button,
   Card,
   Field,
   Flex,
-  Form,
   Heading,
   Input,
   Text,
 } from "rimble-ui";
 
-const User = (props) => {
-  const { recentUsage } = props;
+type UserState = {
+  depositAmount: number,
+  withdrawAmount: number,
+  currentUsage: any
+};
+
+// const web3Context = useWeb3React();
+// const chainlinkContract = new ethers.Contract(
+//   addresses.anyRateOracle,
+//   abis.anyRateOracle,
+//   web3Context.library
+// );
+
+class User extends React.Component<{}, UserState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      depositAmount: 0,
+      withdrawAmount: 0,
+      currentUsage: 0
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://anyrate-sails-api.herokuapp.com/api/usagecount/user1since20210401", {
+      headers: { "Access-Control-Allow-Origin": "*" }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ currentUsage: data.count })
+      });
+  }
+
+  changeDeposit = (e) => {
+    this.setState({ depositAmount: e.target.value });
+  };
+  changeWithdraw = (e) => {
+    this.setState({ withdrawAmount: e.target.value });
+  };
   
-  const [depositAmount, setDepositAmount] = useState("");
-  const [withdrawAmount, setWithdrawAmount] = useState("");
-  const handleDeposit = (e) => {
-    setDepositAmount(e.target.value);
+  submitDeposit = (e) => {
+    console.log('use ethers to deposit');
   };
-  const handleWithdraw = (e) => {
-    setWithdrawAmount(e.target.value);
+  submitWithdraw = (e) => {
+    console.log('use ethers to withdraw');
   };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Submitted form");
-  };
-
-  return (
-    <Flex>
-      <Heading as={"h1"}>User Dashboard</Heading>
-      <Form onSubmit={handleSubmit}>
-        <Field label="Deposit">
-          <Input
-            type="number"
-            required
-            placeholder="000"
-            value={depositAmount}
-            onChange={handleDeposit}
-          />
-        </Field>
-        <Field label="Withdraw">
-          <Input
-            type="number"
-            required
-            placeholder="000"
-            value={withdrawAmount}
-            onChange={handleWithdraw}
-          />
-        </Field>
-        <Text>
-          {/* How do I use fetch from this kind of component? Need it to be a class? */}
-          Recent usage: {recentUsage} 
-        </Text>
-      </Form>
+  
+  render() {
+    const { depositAmount, withdrawAmount, currentUsage } = this.state;
+    
+    return (
+      <Flex alignItems="center" alignContent="center" justifyContent="space-around">
+      <Box width={[9 / 10]} maxWidth={800} marginTop={4}>
+        <Card maxWidth={1000}>
+          <Heading as={"h1"}>User Dashboard</Heading>
+            <Flex>
+              <Flex flexDirection="column" alignItems="flex-start" justifyContent="space-around">
+              <Text>All values in ETH</Text>
+              <Field label="Deposit">
+                <Input
+                  type="number"
+                  required
+                  placeholder="00"
+                  value={depositAmount}
+                  onChange={this.changeDeposit}
+                  />
+              </Field>
+              <Button onClick={this.submitDeposit}>Deposit</Button>
+              <Field label="Withdraw">
+                <Input
+                  type="number"
+                  required
+                  placeholder="00"
+                  value={withdrawAmount}
+                  onChange={this.changeWithdraw}
+                  />
+              </Field>
+              <Button onClick={this.submitWithdraw}>Withdraw</Button>
+            </Flex>
+            <Flex flexDirection="column" alignItems="flex-start" justifyContent="space-around">
+              <Text>
+                Current usage: {currentUsage} 
+              </Text>
+            </Flex>
+          </Flex>
+        </Card>
+      </Box>
     </Flex>
   )
+}
 }
 
 export default User;
