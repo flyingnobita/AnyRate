30f98b7afaba1543e11fb0cff821330cca3fa6de
Sam Hodak <slhodak@gmail.com>
2021-04-11 02:08:49 -0700

    Allow treasury factory to get balances of treasuries, add address ID to treasury, test

--- BillingSystem/test/treasuryFactory.js
+++ BillingSystem/test/treasuryFactory.js
@@ -1,13 +1,28 @@
 const { expect } = require("chai");
 
+before(async () => {
+  TreasuryFactory = await ethers.getContractFactory("TreasuryFactory");
+  treasuryFactory = await TreasuryFactory.deploy();
+  [accountOne, accountTwo] = await ethers.getSigners();
+  treasuryName = "Gumbles";
+});
+
 describe("TreasuryFactory", function() {
   it("Should be able to create a named treasury", async () => {
-    const TreasuryFactory = await ethers.getContractFactory("TreasuryFactory");
-    const treasuryFactory = await TreasuryFactory.deploy();
-    await treasuryFactory.deployed();
-    await treasuryFactory.createTreasury("FactoryTest");
-    await treasuryFactory.treasuries("FactoryTest");
-    const name = await treasuryFactory.callName("FactoryTest");
-    expect(name).to.eq("FactoryTest");
+    await treasuryFactory.createTreasury(treasuryName);
+    expect(await treasuryFactory.callName(treasuryName)).to.eq(treasuryName);
+  });
+  it("Should reveal the balance of a treasury by name", async () => {
+    await treasuryFactory.createTreasury(treasuryName);
+    const treasury = await treasuryFactory.treasuries(treasuryName);
+    await accountOne.sendTransaction({ to: treasury, value: ethers.utils.parseEther("10.0") });
+    expect((await treasuryFactory.balanceOf(treasuryName)).toString()).to.eq(ethers.utils.parseEther("10.0").toString());
+  });
+  it("Should allow clients to transfer value out of their treasuries", async () => {
+    await treasuryFactory.createTreasury(treasuryName);
+    const treasury = await treasuryFactory.treasuries(treasuryName);
+    await accountOne.sendTransaction({ to: treasury, value: ethers.utils.parseEther("10.0") });
+    await treasuryFactory.callTransferTo(treasuryName, accountTwo.address, ethers.utils.parseEther("1.0"));
+    expect((await treasuryFactory.balanceOf(treasuryName)).toString()).to.eq(ethers.utils.parseEther("9.0").toString());
   });
 });
